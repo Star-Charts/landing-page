9679c4c022ca7f7ee4980e0d6efeb015da898222
Ashley Owens <ash4741@gmail.com>
2020-04-09 21:03:50 -0400

    SVG Test 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import bg from './bgDecoration-01.svg';
+import bg from './bgDecoration.svg';
 import Header from './components/Header';
 
 function App() {
