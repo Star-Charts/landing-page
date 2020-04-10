7cab05ca82091ea380c1bb9f0cc75f572de034d0
Ashley Owens <ash4741@gmail.com>
2020-04-09 20:09:03 -0400

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Header from './components/Header';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Route path='/' component={Header} />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import logo from '../spacequestlogo-03.svg';
+import styled from 'styled-components';
+
+const HeaderStyles = styled.div `
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-right: 2rem;
+
+  img {
+    width: 6rem;
+  }
+    
+  .button div:nth-child(1) {
+    border-radius: 0.25rem 0 0 0.25rem;
+    border-right: 0;
+  }
+  
+  .button div {
+    color: #4E3F99; /*Medium purple*/
+    text-align: center;
+    font-weight: 500;
+    display: inline-block;
+    background: transparent;
+    padding: .3rem 0;
+    width: 6rem;
+    border: 2px solid #222050; /*Darker purple*/
+    border-radius: 0 0.25rem 0.25rem 0;
+    transition: background-color .2s;
+
+    :hover {
+      background-color: #222050; /*Darker purple*/
+    }
+
+  }
+
+
+
+`;
 
 function Header() {
   return (
-    <div className="App">
-      <p>HI this is landing page</p>
-    </div>
+    <HeaderStyles>
+      <img src={logo} alt='logo' />
+      <div className='button'>
+        <div>Login</div>
+        <div>Register</div>
+      </div>
+    </HeaderStyles>
   );
 }
 
